a0ecca86421c69b89e1649e8a69da13ffb520dce
hashiva <hashiba@serverworks.co.jp>
2011-02-26 12:57:09 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashiba::Application.config.secret_token = '5436ae14065e6010b880e5aea479e8ee31422483dba1b0f370c173ccd612d94d3315a76c51286983190fe78ac4d2605befe63847fd4cced31eeaf45d8e1dc4d5'
